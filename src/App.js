cf8260f62ce157f5716f069e5246f6f35d9004ea
Shubham-pb08 <110660618+Shubham-pb08@users.noreply.github.com>
2023-06-08 09:15:49 -0400

    bug fix check

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const openai = new OpenAIApi(configuration);
 const completion = await openai.createChatCompletion({
   model: "gpt-3.5-turbo",
   messages: [{role: "user", content: "Hello world"}],
+  Authorization: "Bearer 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"
 });
   console.log(completion.data.choices[0].message);
 
